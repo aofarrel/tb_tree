5e2963df86ffadef9255631fd9a1b5d76f22ef74
lilymaryam <lmkarim@ucdavis.edu>
2022-10-10 13:24:20 -0700

    latest version

--- cut-megafile-sept.py
+++ cut-megafile-sept.py
@@ -1,13 +1,12 @@
 import os
 import argparse
 import gzip
-#import merged_to_diff_sept as mtd
 
 parser = argparse.ArgumentParser()
 parser.add_argument('-v', '--megaVCF', required=True, type=str,help='path to giant VCF to be chunked')
 parser.add_argument('-d', '--workingDirectory', required=True, type=str, help='directory for all outputs (make sure this directory will have enough space!!!!)')
-parser.add_argument('-t', '--threads', type=int, help='number of threads (where applicable)', default=1)
-parser.add_argument('-sd', '--scriptsDirectory', type=str, required=True, help='path to directory where scripts are')
+#parser.add_argument('-t', '--threads', type=int, help='number of threads (where applicable)', default=1)
+#parser.add_argument('-sd', '--scriptsDirectory', type=str, required=True, help='path to directory where scripts are')
 
 args = parser.parse_args()
 vcf = args.megaVCF
@@ -17,16 +16,17 @@ wd = args.workingDirectory
 if wd[-1] != '/':
     #print('add a final slash')
     wd = wd+'/'
-sd = args.scriptsDirectory
-if sd[-1] != '/':
+#sd = args.scriptsDirectory
+#if sd[-1] != '/':
     #print('add a final slash')
-    sd = sd+'/'
+#    sd = sd+'/'
 
-t = args.threads
+#t = args.threads
 
                             
 def find_snps(line):
-    print('snps', line)
+    #for lines where len(ref)==len(alt), look for snps instead of processing as one large chunk
+
     ref = line[3]
     alt = line[4]
     end = len(ref)
@@ -34,9 +34,7 @@ def find_snps(line):
     for i in range(end):
         if ref[i] != alt[i]:
             snps.append(i)
-    
-        
-    #print('snps', snps)
+    #generate new lines for diff file
     lines = []
     for s in range(len(snps)):
         lines.append([alt[snps[s]], str(int(line[1])+snps[s]), '1'])
@@ -44,33 +42,33 @@ def find_snps(line):
     return lines
 
 def process_dels(line):
-    print('dels', line)
-    #lines = []
+    #for lines where len(ref) > 1 AND len(alt) == 1, these deletions are easily processed 
+    #currently deletions and missing data are all converted to '-'
     ref = line[3]
     alt = line[4]
     end = len(ref)
     assert len(alt) == 1
+
+    #make sure remaining alt nucleotide is the same as the corresponding ref nuc
     if alt[0] == ref[0]:
-        print(ref, alt, end)
-        #gap or N? 
-        #make sure the alignemnt is correct for this 
         l = ['-', str(int(line[1])+1), str(end-1)]
 
     
     elif alt[0]  == '-':
+        #this is for missing data
         l = ['-', line[1], str(end)]
 
     else:
+        #this is a scenario that could be represented by a snp at the first ref position
+        #this can be changed 
         l = ['-', line[1], str(len(line[3]))]
-        print('wtf', line)
-
-    #print('lines', line)
+        
     return l
 
-#currently I will mask the whole region and then figure it out later 
+
 
 def process_others(line):
-    print('others', line)
+    #for sitations where reference and alt do not align 
     ref = line[3]
     alt = line[4]
     end = len(ref)
@@ -79,12 +77,11 @@ def process_others(line):
     if ref > alt:
         l = ['-', line[1], str(end)]
 
-    #print('weird',line)
     return l
 
 
 def vcf_to_diff(vcf_file, output):
-
+    #takes a single sample vcf and converts to diff format 
     with gzip.open(vcf_file, 'rt') as v:
         with open(output, 'w') as o:
             missing = 0
@@ -94,7 +91,7 @@ def vcf_to_diff(vcf_file, output):
                     if line.startswith('#'):
                         line = line.strip().split()
                         sample = line[-1]
-                        print('sample', sample)
+                        #print('sample', sample)
                         o.write(f'>{sample}\n')
                     else:
                         total += 1
@@ -104,7 +101,7 @@ def vcf_to_diff(vcf_file, output):
                         if var != '0/0':
                             
                             if var == './.':
-                                print('missing', line)
+                                #print('missing', line)
                                 missing += 1
                                 line[4] = '-'
                                 line [-1] = '1'
@@ -113,19 +110,12 @@ def vcf_to_diff(vcf_file, output):
                                 
                                 var = var.split('/')
                                 var = var[0]
-                                #print(var)
                                 alts = line[4].split(',')
-                                #if len(alts) > 1:
-                                #    print(line[1],line[3],alts,var)
                                 alt = alts[int(var)-1]
-                                #print(alt)
                                 line[4] = alt
                                 line[-1] = '1'
 
-                            
-
-                            
-                            print('line', line)
+                            #print('line', line)
                             assert type(line[4]) == str
                             if len(line[3]) == 1:
 
@@ -146,7 +136,7 @@ def vcf_to_diff(vcf_file, output):
                                 elif len(line[4]) == 1:
                                     #print('deletion')
                                     newline = process_dels(line)
-                                    print(newline)
+                                    #print(newline)
                                     o.write('\t'.join(newline)+'\n')
 
                                 else:
@@ -160,6 +150,50 @@ def vcf_to_diff(vcf_file, output):
             print('missing', missing, 'total', total)
 
     return sample
+
+
+
+def squish(file):
+    #condenses diff entries that can be 
+    with open(file) as f:
+        with open(f'{file}squish', 'w') as o:
+
+            prev = None
+            for line in f:
+                if not line.startswith('>'):
+
+                    #process line
+                    line = line.strip().split()
+                    #print(line, prev)
+                    
+                    if prev != None:
+                        #print('line', line)
+                        #print('prev', prev)
+                        if prev[0] == line[0]:
+                            #print('prev', prev, 'line', line)
+                            if int(prev[1]) == int(line[1])-int(prev[2]):
+                                #print('prev', prev, 'now', line)
+                                prev[2] = str(int(prev[2])+int(line[2]))
+                                #print('new prev', prev)
+                            else:
+                                o.write('\t'.join(prev)+'\n') 
+                                prev = line
+                        else:
+                            o.write('\t'.join(prev)+'\n') 
+                            prev = line
+                        
+                    
+                    #the first line of file becomes prev variable 
+                    else:
+                        prev = line
+                        #print('first prev!', prev)
+                        
+                        
+            
+
+                #write header to new file
+                else:
+                    o.write(line)
 
                             
                             
@@ -192,13 +226,23 @@ with gzip.open(vcf, 'rt') as v:
 #change this loop after testing 
 
 #for i in range(10,lenRow):
-for i in range(10,16):
+for i in range(10,lenRow):
     print(i)
-
     os.system(f'gzip -dc {vcf} | cut -f1-9,{i} > {wd}col{i}.vcf')
     os.system(f'bgzip -f {wd}col{i}.vcf')
     os.system(f"bcftools annotate -x '^FORMAT/GT' -O v -o {wd}col{i}filt.vcf {wd}col{i}.vcf.gz")
     os.system(f'bgzip -f {wd}col{i}filt.vcf')
     sample = vcf_to_diff(f'{wd}col{i}filt.vcf.gz', f'{wd}col{i}.diff')
-    os.system(f'mv {wd}col{i}.diff {wd}{sample}.diff')
+    print(sample)
+    os.system(f'rm {wd}col{i}*.vcf.gz')
+    squish(f'{wd}col{i}.diff')
+    os.system(f'rm {wd}col{i}.diff')
+    if '/' not in sample:
+        os.system(f'mv {wd}col{i}.diffsquish {wd}{sample}.diff')
+    else:
+        newname = sample.replace('/', '-')
+        print(newname)
+        os.system(f'mv {wd}col{i}.diffsquish {wd}{newname}.diff')
+        
+
     #vcf_to_diff(f'{wd}col{i}filt.vcf', i, f'{wd}col{i}.diff')
